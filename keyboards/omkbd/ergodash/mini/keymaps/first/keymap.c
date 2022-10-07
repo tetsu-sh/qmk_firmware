f8e689ea55a73aa63277f502c9fc8e9be8909eaf
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2022-10-07 15:41:47 +0900

    create mykeymap

--- keyboards/omkbd/ergodash/mini/keymaps/first/keymap.c
+++ keyboards/omkbd/ergodash/mini/keymaps/first/keymap.c
@@ -32,8 +32,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        KC_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,                        KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LALT ,                        KC_ENT , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    KC_LCTL, KC_LGUI, KC_LALT, KANA,             LOWER,   KC_LGUI , KC_RSFT,        KC_SPC,KC_LCTL , RAISE,            KC_QUOT, KC_BSLS, KC_TILD,   KC_DEL
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KANA ,                        KC_RSFT , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    KC_LCTL, KC_LGUI, KC_LALT, KC_LALT,             KC_SPC,   LOWER , KC_LSFT,        KC_LGUI,KC_LCTL , RAISE,            KC_QUOT, KC_BSLS, KC_TILD,   KC_DEL
   ),
 
   /* Lower
@@ -48,14 +48,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_LOWER] = LAYOUT(
-    KC_GRV , KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,                        KC_PLUS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-    KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LCBR,                        KC_RCBR, KC_6, KC_7, KC_8  , KC_9, KC_0, KC_DQT ,
-    KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_SPC ,                        KC_ENT , KC_N,    KC_M,    KC_LT,   KC_GT,   KC_QUES, KC_RSFT,
-    KC_LCTL, KC_LGUI, KC_LALT, EISU,             LOWER,   KC_SPC , KC_DEL,        KC_RSFT,KC_ENT , RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END
+    KC_GRV , KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_UNDS,                        KC_PLUS, KC_CIRC, KC_AMPR, KC_ASTR, KC_QUOT, KC_BSLS, KC_PIPE,
+    KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LCBR,                        KC_RCBR, KC_6, KC_7, KC_8  , KC_9, KC_0, KC_TILD ,
+    KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_SPC ,                        KC_ENT , KC_LBRC,    KC_RBRC,    KC_LT,   KC_GT,   KC_QUES, KC_RSFT,
+    KC_LCTL, KC_LGUI, KC_LALT, KANA,             KC_SPC,   LOWER , KC_LSFT,        KC_LGUI,KC_LCTL , RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END
   ),
 
   /* Raise
-  * ,----------------------------------------------------------------------------------------------------------------------.
+  * 
   * |   `  |   !  |   @  |   #  |   $  |   %  |   _  |                    |   +  |   ^  |   &  |   *  |   (  |   )  |  |   |
   * |------+------+------+------+------+------+------+--------------------+------+------+------+------+------+------+------|
   * |   ~  |   F1 |   F2 |   F3 |   F4 |   F5 |   {  |                    |   }  | Left | Down |  Up  | Right|   :  |  "   |
@@ -66,10 +66,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * ,----------------------------------------------------------------------------------------------------------------------.
   */
   [_RAISE] = LAYOUT(
-    KC_GRV , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                        KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_PIPE,
-    KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                        KC_RCBR, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_F12, KC_DQT ,
-    KC_LSFT, KC_EXLM,   KC_AT,   KC_HASH,   KC_DLR,   KC_PERC,  KC_SPC ,                        KC_ENT , KC_CIRC,    KC_AMPR,    KC_ASTR,   KC_LPRN,   KC_RPRN, KC_RSFT,
-    KC_LCTL, KC_F11 , KC_F12,  EISU,             LOWER,   KC_SPC , KC_DEL,        KC_BSPC,KC_ENT , RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END
+    KC_GRV , KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                        KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL,
+    KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                        KC_RCBR, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_MINS, KC_EQL ,
+    KC_LSFT, KC_EXLM,   KC_AT,   KC_HASH,   KC_DLR,   KC_PERC,  KC_LALT ,                        KC_ENT , KC_LCBR,    KC_RCBR,    KC_LPRN,   KC_RPRN,   KC_QUES, KC_RSFT,
+    KC_LCTL, KC_LGUI , KC_LALT,  KANA,             KC_SPC,   LOWER , KC_LSFT,        KC_LGUI,KC_LCTL , RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END
     ),
 
   /* Adjust
