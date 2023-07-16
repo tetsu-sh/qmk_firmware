0d63637b8380879db0506f91a185a10c2a0e93d0
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2023-07-16 17:29:11 +0900

    adjust with chary

--- keyboards/omkbd/ergodash/mini/keymaps/first/keymap.c
+++ keyboards/omkbd/ergodash/mini/keymaps/first/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_LOWER] = LAYOUT(
     KC_GRV , KC_F1, KC_F2,   KC_F3, KC_F4,  KC_F5, KC_F6,                        KC_F6, KC_F6, KC_F7, KC_F8, KC_F9, KC_F12, KC_PIPE,
-    KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LCBR,                        KC_RCBR, KC_6, KC_7, KC_8  , KC_9, KC_0, KC_TILD ,
+    KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LCBR,                        KC_RCBR, KC_6, KC_7, KC_8  , KC_9, KC_0, KC_QUOT ,
     KC_LSFT, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_SPC ,                        KC_ENT , KC_LBRC,    KC_RBRC,    KC_LT,   KC_GT,   KC_QUES, KC_RSFT,
     KC_LCTL, KC_LGUI, KC_LALT, KANA,             KC_SPC,   LOWER , KC_LSFT,        KC_LGUI,KC_LCTL , RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END
   ),
@@ -66,10 +66,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * ,----------------------------------------------------------------------------------------------------------------------.
   */
   [_RAISE] = LAYOUT(
-    KC_GRV , KC_1,   KC_F,   KC_3,   KC_4,   KC_5,   KC_F6,                        KC_F6, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL,
+    KC_GRV , KC_1,   KC_F,   KC_3,   KC_4,   KC_5,   KC_F6,                        KC_F6, KC_6, KC_7, KC_QUOT, KC_BSLS, KC_TILD, KC_DEL,
     KC_TAB, KC_F1,   KC_J,   KC_LCTL,   KC_LGUI,   KC_K,   KC_F6,                        KC_RCBR, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_MINS, KC_EQL ,
     KC_LSFT, KC_1,   KC_2,   KC_3,   KC_4,   KC_5,  KC_LALT ,                        KC_ENT , KC_LCBR,    KC_RCBR,    KC_LPRN,   KC_RPRN,   KC_QUES, KC_RSFT,
-    KC_LCTL, KC_LGUI , KC_LALT,  KANA,             KC_LSFT,   LOWER , KC_LSFT,        KC_LGUI,KC_LCTL , RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END
+    KC_LCTL, KC_LGUI , KC_LALT,  KANA,             KC_LALT,   LOWER , KC_LSFT,        KC_LGUI,KC_LCTL , RAISE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END
     ),
 
   /* Adjust
