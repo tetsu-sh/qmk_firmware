91716f7a4e9f5fa13ba404ec760f6da5bc6b4440
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2023-07-23 22:20:05 +0900

    fix

--- keyboards/bastardkb/charybdis/4x6/keymaps/mykey/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/mykey/keymap.c
@@ -78,8 +78,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────┤
        KC_LGUI,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,       KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, TG_POINTER,
   // ╰──────────────────────────────────────────────────────┤ ├──────────────────────────────────────────────────────╯
-                                   KC_SPC, LOWER,   KC_LSFT,     KC_RCTL,  RAISE,
-                                           POINTER, KC_LALT,     KC_RGUI
+                                   KC_SPC, LOWER,   KC_LSFT,     KC_RGUI,  RAISE,
+                                           POINTER, KC_LALT,     KC_RCTL
   //                            ╰───────────────────────────╯ ╰──────────────────╯
   ),
 
