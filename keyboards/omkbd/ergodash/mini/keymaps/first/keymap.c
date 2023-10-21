a760cf9ea9b50d2be25af4f2b2cfce19571c79eb
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2023-10-21 22:06:10 +0900

    update

--- keyboards/omkbd/ergodash/mini/keymaps/first/keymap.c
+++ keyboards/omkbd/ergodash/mini/keymaps/first/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT(
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        KC_BTN2 , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,                        KC_BTN1, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KANA ,                        KC_LGUI, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, LOWER,
+    KC_LGUI, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_BTN5 ,                        KC_LGUI, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, LOWER,
     KC_LCTL, KC_LGUI, KC_LALT, KC_LALT,             KC_SPC,   LOWER , KC_LSFT,        KC_LGUI,KC_LCTL , RAISE,            KC_QUOT, KC_BSLS, KC_TILD,   KC_DEL
   ),
 
