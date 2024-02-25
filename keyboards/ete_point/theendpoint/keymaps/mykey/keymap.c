a9aed682a1efa3ac1b27b935ce52b998dc933f49
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2024-02-25 22:12:34 +0900

    update

--- keyboards/ete_point/theendpoint/keymaps/mykey/keymap.c
+++ keyboards/ete_point/theendpoint/keymaps/mykey/keymap.c
@@ -21,6 +21,7 @@ enum ETE_keycodes {
     SCRL_MO, // Momentary scroll mode
     SCRL_DVI, // Increment scroll divider
     SCRL_DVD, // Decrement scroll divider
+    KANA,
 };
 
 #define REC_RST QK_KB_0
@@ -39,25 +40,25 @@ enum ETE_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
    //,---------------------------------------------------------------------.     ,-----------------------------------------------------------------------.
-      KC_ESC,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_MUTE, KC_SPC,       KC_ENT,  KC_TAB,  KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,     KC_MINS,
+      KC_ESC,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_MUTE, MO(1),       KC_RCTL,  KC_BTN2,  KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,     KC_BSPC,
   //|--------+--------+--------+--------+--------+--------+--------+---------|  |--------+--------+--------+--------+--------+--------+--------+---------|
-      KC_LSFT, KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_HOME, KC_PGDN,      KC_UP,   KC_END,  KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,  KC_COLN,
+      KC_TAB, KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    SCRL_MO,   KC_LALT,      KC_RGUI,  KC_BTN1,  KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,  KC_ENT,
   //|--------+--------+--------+--------+--------+--------+--------+---------|  |--------+--------+--------+--------+--------+--------+--------+---------|
-      KC_LCTL, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    KC_END,  KC_END,       KC_LEFT, KC_RGHT, KC_N,    KC_M,   KC_COMM,  KC_DOT, KC_SLSH,  KC_BSLS,
+      KC_LGUI, KC_Z,    KC_X,    KC_C,   KC_V,    KC_B,    SCRL_MO,   KC_LSFT,     KC_LEFT, KC_RCTL, KC_N,    KC_M,   KC_COMM,  KC_DOT, KC_SLSH,  KANA,
   //|--------+--------+--------+--------+--------+--------+--------+---------|  |--------+--------+--------+--------+--------+--------+--------+---------|
-      KC_TAB,  KC_LALT, KC_LGUI, KC_PSCR,         MO(1),   KC_DEL,  KC_HOME,      KC_END,  KC_DOWN, MO(2) ,          KC_BSPC,  KC_DEL, KC_RALT,  KC_AT,
+      KC_LGUI, KC_LCTL, KC_LALT, KC_LSFT,         KC_SPC,   KC_LSFT,  KC_LALT,      KC_END,  KC_DOWN, MO(2) ,          SCRL_MO,  KC_LSFT, KC_RALT,  KC_SPC,
   //|--------+--------+--------+--------+--------+--------+--------+---------|  |--------+--------+--------+--------+--------+--------+--------+---------|
       KC_PGUP,KC_PGDN,                                                                                                                  KC_LEFT, KC_RIGHT
   ),
     [1] = LAYOUT(
   //,----------------------------------------------------------------------.     ,-----------------------------------------------------------------------.
-      KC_CAPS,KC_CAPS, KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,  KC_1,   KC_2,    KC_3,    KC_HOME , KC_UP,   KC_END,
+      KC_ESC,  KC_F1,    KC_F2,  KC_F3,    KC_F4,   KC_F5,   KC_TRNS, KC_TRNS,     KC_RCTL, KC_TRNS,  KC_F6,   KC_F7,  KC_F8,   KC_F10, KC_F12,   KC_BSPC,
   //|--------+--------+--------+--------+--------+--------+--------+---------|  |--------+--------+--------+--------+--------+--------+--------+---------|
-      KC_TRNS,KC_TRNS, KC_TRNS, KC_F4,   KC_F5,   KC_F6,   KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,  KC_4,   KC_5,    KC_6,    KC_LEFT,  KC_DOWN, KC_RGHT,
+      KC_TILD,KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_SPC, KC_LALT,      KC_RGUI, KC_TRNS,  KC_6,   KC_7,    KC_8,    KC_9,     KC_0,   KC_QUOT,
   //|--------+--------+--------+--------+--------+--------+--------+---------|  |--------+--------+--------+--------+--------+--------+--------+---------|
-      KC_TRNS,KC_TRNS, KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,  KC_7,   KC_8,    KC_9,    KC_0,  KC_MS_WH_UP,KC_MS_WH_DOWN,
+      KC_LGUI,KC_TRNS, KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_LALT, KC_LSFT,     KC_TRNS, KC_RCTL,  KC_LBRC,KC_RBRC, KC_LT,   KC_GT,   KC_PMNS, KC_PEQL,
   //|--------+--------+--------+--------+--------+--------+--------+---------|  |--------+--------+--------+--------+--------+--------+--------+---------|
-      KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,  MO(3),           KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
+      KC_LGUI, KC_LCTL, KC_LALT, KC_LSFT,          KC_SPC, KC_LSFT, KC_LALT,     KC_TRNS, KC_TRNS,  MO(3),           SCRL_MO,  KC_LSFT, KC_RALT,  KC_SPC,
   //|--------+--------+--------+--------+--------+--------+--------+---------|  |--------+--------+--------+--------+--------+--------+--------+---------|
      KC_TRNS, KC_TRNS,                                                                                                                  KC_UP,   KC_DOWN 
   //|--------+--------+--------+--------+--------+--------+--------+---------.  ,--------+-------+--------+--------+--------+---------+--------+---------'
@@ -65,26 +66,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [2] = LAYOUT(
   //,------------------------------------------------------------------------.    ,----------------------------------------------------------------------.
-     KC_ESC,  KC_EXLM, KC_AT,   KC_HASH,  KC_DLR,  KC_PERC, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_CIRC, KC_QUOT, KC_DQUO, KC_LPRN, KC_RPRN, KC_UNDS,
+     KC_ESC,  KC_F1,    KC_F2,  KC_F3,    KC_F4,   KC_F5,   KC_TRNS, MO(3),    KC_RCTL, KC_TRNS, KC_F6,   KC_F7,  KC_QUOT,   KC_BSLS, KC_TILD, KC_DEL,
   //|--------+--------+--------+--------+--------+--------+--------+---------|  |--------+--------+--------+-------+--------+--------+--------+---------|
-     KC_LSFT, KC_AT,   KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS, KC_LBRC, KC_RCBR, KC_TILD,
+     KC_LSFT, KC_AT,   KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_SPC, KC_LALT,      KC_RGUI, KC_TRNS, KC_LEFT, KC_DOWN,KC_UP,   KC_RIGHT,KC_MINS, KC_EQL,
   //|--------+--------+--------+--------+--------+--------+--------+---------|  |--------+--------+--------+-------+--------+--------+--------+---------|
-     KC_LCTL, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS, KC_LCBR, KC_RCBR, KC_PIPE,
+     KC_LGUI, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_LALT, KC_LSFT,    KC_TRNS, KC_RCTL, KC_LCBR, KC_RCBR,KC_LPRN, KC_RPRN, KANA,    KANA,
   //|--------+--------+--------+--------+--------+--------+--------+---------|  |--------+--------+--------+-------+--------+--------+--------+---------|
-     KC_TAB,  KC_TRNS, KC_TRNS, KC_TRNS,            MO(3),  KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+     KC_LGUI, KC_LCTL, KC_LALT, KC_LSFT,            KC_SPC,  KC_LSFT,   KC_LALT,    KC_TRNS, KC_TRNS, KC_TRNS,         SCRL_MO,  KC_LSFT, KC_RALT,  KC_SPC,
   //|--------+--------+--------+--------+--------+--------+--------+---------|  |--------+--------+--------+-------+--------+--------+--------+---------|
      KC_MS_WH_LEFT,KC_MS_WH_RIGHT,                                                                                                    KC_BSPC, KC_DEL  
   //|--------+--------+--------+--------+--------+--------+--------+---------.  ,---------+-------+--------+-------+--------+--------+--------+---------'
   ),
    [3] = LAYOUT(
 //,-------------------------------------------------------------------------.    ,-----------------------------------------------------------------------.
-     RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, REC_RST, KC_TRNS, SCRL_DVI, SCRL_MO,    SCRL_TO, KC_TRNS, KC_TRNS, KC_VOLU,  KC_BRIU, KC_MS_BTN1,KC_MS_UP,KC_MS_BTN2,
+     RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, REC_RST, KC_TRNS, SCRL_DVI, SCRL_MO,    KC_RCTL, KC_TRNS, CPI_I100,CPI_D100,SCRL_DVD,SCRL_DVI,REC_SAVE,KC_MS_BTN2,
   //|--------+--------+--------+--------+--------+--------+--------+---------|  |--------+--------+--------+--------+--------+--------+--------+---------|
-     RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, REC_SAVE,KC_MS_BTN2,SCRL_DVD, KC_TRNS,  KC_TRNS, KC_TRNS, KC_MS_BTN2,KC_VOLD,KC_BRID, KC_MS_LEFT,KC_MS_D, KC_MS_R,
+     KC_TILD, KC_EXLM, KC_AT,  KC_HASH, KC_DLR,   KC_PERC, KC_SPC, KC_LALT,      KC_RGUI, KC_TRNS, KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN,KC_RPRN, KC_UNDS,
   //|--------+--------+--------+--------+--------+--------+--------+---------|  |--------+--------+--------+--------+--------+--------+--------+---------|
-     KC_TRNS, KC_TRNS,CPI_I100,CPI_D100,KC_TRNS,  KC_MS_BTN2, KC_MS_BTN1,KC_TRNS,KC_TRNS, KC_MS_BTN1,KC_MS_BTN2,KC_MUTE, KC_TRNS, KC_TRNS,KC_MS_WH_UP,KC_MS_WH_DOWN,
+     KC_LGUI, KC_TRNS,CPI_I100,CPI_D100,KC_TRNS,  KC_MS_BTN2, KC_LALT,KC_LSFT,   KC_TRNS,KC_MS_BTN1,KC_BTN5,KC_BTN1, KC_VOLD, KC_VOLU,KC_MUTE,KC_MS_WH_DOWN,
   //|--------+--------+--------+--------+--------+--------+--------+---------|  |--------+--------+--------+--------+--------+--------+--------+---------|
-     KC_TRNS, KC_TRNS,CPI_I1K, CPI_D1K,           KC_TRNS,  KC_TRNS, KC_MS_BTN1, KC_MS_BTN1,KC_TRNS,KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+     KC_LGUI, KC_LCTL, KC_LALT, KC_LSFT,           KC_SPC,  KC_LSFT, KC_LALT,    KC_MS_BTN1,KC_TRNS,KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------+--------+---------|  |--------+--------+--------+--------+--------+--------+--------+---------|
      KC_TRNS, KC_TRNS,                                                                                                         KC_MS_WH_UP,KC_MS_WH_DOWN
   //|--------+--------+--------+--------+--------+--------+-------+----------.   ,-------+--------+--------+--------+--------+--------+--------+---------'
@@ -137,4 +138,20 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
        tap_code16(keycode);
 }
 return false; 
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case KANA:
+      if (record->event.pressed){
+        // tap_code(KC_LCTL);
+        // tap_code(KC_SPC);
+        register_code(KC_LCTL);
+        register_code(KC_SPC);
+        unregister_code(KC_LCTL);
+        unregister_code(KC_SPC);
+      }
+      return false;
+      }
+  return true;
 }
