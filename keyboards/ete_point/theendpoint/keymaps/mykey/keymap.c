dbf4b8a32ca5d44f0135cc3e37fea0591dc7e93b
tetsu-sh <tetsusinmyou@yahoo.co.jp>
2024-02-26 08:58:43 +0900

    update

--- keyboards/ete_point/theendpoint/keymaps/mykey/keymap.c
+++ keyboards/ete_point/theendpoint/keymaps/mykey/keymap.c
@@ -40,7 +40,7 @@ enum ETE_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
    //,---------------------------------------------------------------------.     ,-----------------------------------------------------------------------.
-      KC_ESC,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_MUTE, MO(1),       KC_RCTL,  KC_BTN2,  KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,     KC_BSPC,
+      KC_ESC,  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_MUTE, MO(1),       KC_RGUI,  KC_BTN2,  KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,     KC_BSPC,
   //|--------+--------+--------+--------+--------+--------+--------+---------|  |--------+--------+--------+--------+--------+--------+--------+---------|
       KC_TAB, KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    SCRL_MO,   KC_LALT,      KC_RGUI,  KC_BTN1,  KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,  KC_ENT,
   //|--------+--------+--------+--------+--------+--------+--------+---------|  |--------+--------+--------+--------+--------+--------+--------+---------|
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
     [1] = LAYOUT(
   //,----------------------------------------------------------------------.     ,-----------------------------------------------------------------------.
-      KC_ESC,  KC_F1,    KC_F2,  KC_F3,    KC_F4,   KC_F5,   KC_TRNS, KC_TRNS,     KC_RCTL, KC_TRNS,  KC_F6,   KC_F7,  KC_F8,   KC_F10, KC_F12,   KC_BSPC,
+      KC_ESC,  KC_F1,    KC_F2,  KC_F3,    KC_F4,   KC_F5,   KC_TRNS, KC_TRNS,     KC_RGUI, KC_TRNS,  KC_F6,   KC_F7,  KC_F8,   KC_F10, KC_F12,   KC_BSPC,
   //|--------+--------+--------+--------+--------+--------+--------+---------|  |--------+--------+--------+--------+--------+--------+--------+---------|
       KC_TILD,KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_SPC, KC_LALT,      KC_RGUI, KC_TRNS,  KC_6,   KC_7,    KC_8,    KC_9,     KC_0,   KC_QUOT,
   //|--------+--------+--------+--------+--------+--------+--------+---------|  |--------+--------+--------+--------+--------+--------+--------+---------|
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [2] = LAYOUT(
   //,------------------------------------------------------------------------.    ,----------------------------------------------------------------------.
-     KC_ESC,  KC_F1,    KC_F2,  KC_F3,    KC_F4,   KC_F5,   KC_TRNS, MO(3),    KC_RCTL, KC_TRNS, KC_F6,   KC_F7,  KC_QUOT,   KC_BSLS, KC_TILD, KC_DEL,
+     KC_ESC,  KC_F1,    KC_F2,  KC_F3,    KC_F4,   KC_F5,   KC_TRNS, MO(3),    KC_RGUI, KC_TRNS, KC_F6,   KC_F7,  KC_QUOT,   KC_BSLS, KC_TILD, KC_DEL,
   //|--------+--------+--------+--------+--------+--------+--------+---------|  |--------+--------+--------+-------+--------+--------+--------+---------|
      KC_LSFT, KC_AT,   KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_SPC, KC_LALT,      KC_RGUI, KC_TRNS, KC_LEFT, KC_DOWN,KC_UP,   KC_RIGHT,KC_MINS, KC_EQL,
   //|--------+--------+--------+--------+--------+--------+--------+---------|  |--------+--------+--------+-------+--------+--------+--------+---------|
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
    [3] = LAYOUT(
 //,-------------------------------------------------------------------------.    ,-----------------------------------------------------------------------.
-     RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, REC_RST, KC_TRNS, SCRL_DVI, SCRL_MO,    KC_RCTL, KC_TRNS, CPI_I100,CPI_D100,SCRL_DVD,SCRL_DVI,REC_SAVE,KC_MS_BTN2,
+     RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, REC_RST, KC_TRNS, SCRL_DVI, SCRL_MO,    KC_RGUI, KC_TRNS, CPI_I100,CPI_D100,SCRL_DVD,SCRL_DVI,REC_SAVE,KC_MS_BTN2,
   //|--------+--------+--------+--------+--------+--------+--------+---------|  |--------+--------+--------+--------+--------+--------+--------+---------|
      KC_TILD, KC_EXLM, KC_AT,  KC_HASH, KC_DLR,   KC_PERC, KC_SPC, KC_LALT,      KC_RGUI, KC_TRNS, KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN,KC_RPRN, KC_UNDS,
   //|--------+--------+--------+--------+--------+--------+--------+---------|  |--------+--------+--------+--------+--------+--------+--------+---------|
